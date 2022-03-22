a9d1e1dba3b05cce6098c8f489ebc77ca301297a
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-03-21 21:31:24 -0400

    user kinda working

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 const express = require("express");
 const app = express();
+var faker = require("@faker-js/faker");
 const port = 9000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 class User {
-  constructor() {
-    this._id = 8;
-    this.firstName = "eric";
-    this.lastName = "niemo";
-    this.phoneNumber = "350";
-    this.email = "niemo@gmail.com";
-    this.password = "testpass";
+  //   constructor(_id=8, firstName="eric", lastName="niemo", phoneNumber="80000", email="niemo@gmail.com", password="12345") {
+  // constructor(options = {_id, firstName, lastName, phoneNumber, email, password}) {
+  constructor(options = {_id: "0", firstName: "Roby", lastName: "Daniele", phoneNumber: "1234", email: "roby@gmail.com", password: "asdfasdf"}) {
+    this._id = options._id;
+    this.firstName = options.firstName;
+    this.lastName = options.lastName;
+    this.phoneNumber = options.phoneNumber;
+    this.email = options.email;
+    this.password = options.password;
   }
 }
 
-class Compoany {
+class Company {
   constructor() {
     this._id = 8;
-    this.name = "eric";
-    this.address = "niemo";
-    this.street = {
-      street: "block street",
+    this.name = "google";
+    this.address = {
+      street: "here street",
       city: "NYC",
       state: "New York",
       zipCode: 63808,
@@ -31,8 +33,10 @@ class Compoany {
   }
 }
 
-const eric = new User();
+const eric = new User({id: "3", firstName: "Eric", lastName:"Niemo", phoneNumber: "12345", email: "niemo@gmail.com", password: "1234"})
 const roby = new User();
+const microsoft = new Company();
+const google = new Company();
 
 app.get("/", (req, res) => {
   console.log("newer one");
@@ -57,18 +61,26 @@ app.get("/api/other", (req, res) => {
 
 // USER
 app.get("/api/users/new", (req, res) => {
-  console.log("getting other route");
-  res.json([eric._id, eric.firstName, eric.lastName]);
+  console.log("getting new user");
+  res.json({
+    // Object.key(eric)[0]:
+    _id: eric._id,
+    firstName: eric.firstName,
+    lastName: eric.lastName,
+    phoneNumber: eric.phoneNumber,
+    email: eric.email,
+    password: eric.password,
+  });
 });
 
 // COMPANY
 app.get("/api/company/new", (req, res) => {
-  console.log("getting other route");
-  res.json([
-    "here is item 0",
-    "you can put whatever you want in here!",
-    new Date().getTime(),
-  ]);
+  console.log("getting new company");
+  res.json({
+    _id: microsoft._id,
+    name: microsoft.name,
+    address: microsoft.address,
+  });
 });
 
 app.get("/api/repeat/:word", (req, res) => {
@@ -83,6 +95,4 @@ app.post("/api/repeat", (req, res) => {
 
 ////////////////////
 
-app.listen(port, () =>
-  console.log(`Running on port ${port} is a new way I like to be!`)
-);
+app.listen(port, () => console.log(`Running on port ${port}`));
